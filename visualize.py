fix: coordinate rotation

visualize.py
@@ -7,8 +7,9 @@ from scipy.spatial.transform import Rotation as R
 import tempfile
 from tqdm import tqdm
 
+ROOT_FOLDER='/home/lab525/build/dataset' #'dataset'
 DATASET = 'wei'
-MAX_DEPTH = 5
+MAX_DEPTH = 10
 
 def monochromize(pcd, color):
     num_pcd = len(pcd.points)
@@ -17,26 +18,52 @@ def monochromize(pcd, color):
     pcd.colors = o3d.utility.Vector3dVector(colors)
     pass
 
+def build_coord_geometry():
+    coord_x = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector([ [0,0,0],[10,0,0] ]),
+        lines=o3d.utility.Vector2iVector([ [0,1] ])
+    )
+    coord_x.colors = o3d.utility.Vector3dVector([[1,0,0]])
+    coord_y = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector([ [0,0,0],[0,10,0] ]),
+        lines=o3d.utility.Vector2iVector([ [0,1] ])
+    )
+    coord_y.colors = o3d.utility.Vector3dVector([[0,1,0]])
+    coord_z = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector([ [0,0,0],[0,0,10] ]),
+        lines=o3d.utility.Vector2iVector([ [0,1] ])
+    )
+    coord_z.colors = o3d.utility.Vector3dVector([[0,0,1]])
+    return [coord_x, coord_y, coord_z]
+
 def visualize_chopped_pcd(pcd, chopped_pcd, pyramid):
     monochromize(chopped_pcd, [0,1,0])
     pyramid_geometry = o3d.geometry.LineSet(
         points=o3d.utility.Vector3dVector(pyramid),
         lines=o3d.utility.Vector2iVector([[0,1],[0,2],[0,3],[0,4],[1,3],[1,4],[2,3],[2,4], [1,2],[3,4]])
     )
-    o3d.visualization.draw_geometries([pcd, chopped_pcd, pyramid_geometry])
+    coord = build_coord_geometry()
+    o3d.visualization.draw_geometries([pcd, chopped_pcd, pyramid_geometry, *coord])
     pass
 
-## load point cloud
-print('load point cloud ...')
-PLY_PATH = 'dataset/%s/sparse/0/points3D.ply'%DATASET
-ply_pcd = o3d.io.read_point_cloud(PLY_PATH)
-pcd = o3d.geometry.PointCloud()
-pcd.points = ply_pcd.points
-pcd.colors = ply_pcd.colors
+def visualize_trajectory(pcd, images):
+    images = images
+    # build camera position
+    cameras = np.array([ R.from_quat(x['quaternion']).inv().apply(-np.array(x['translation'])) for x in images ])
+    # build trajectory geometry
+    trajectory_geometry = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector(cameras),
+        lines=o3d.utility.Vector2iVector([ [i-1,i] for i in range(1,len(cameras)) ]),
+    )
+    trajectory_geometry.colors = o3d.utility.Vector3dVector([ [0,1,1] if i%2==0 else [1,0,1] for i in range(len(cameras)-1) ])
+    coord = build_coord_geometry()
+    # plot
+    o3d.visualization.draw_geometries([pcd, trajectory_geometry, *coord])
+    pass
 
 ## load camera parameters
 print('load camera parameters ...')
-CAMERA_PATH = 'dataset/%s/sparse/0/cameras.txt'%DATASET
+CAMERA_PATH = f'{ROOT_FOLDER}/{DATASET}/sparse/0/cameras.txt'
 cameras = dict()
 with open(CAMERA_PATH) as fp:
     for line in fp.readlines():
@@ -59,7 +86,7 @@ with open(CAMERA_PATH) as fp:
 
 ## load camera view
 print('load camera images ...')
-IMAGE_PATH = 'dataset/%s/sparse/0/images.txt'%DATASET
+IMAGE_PATH = f'{ROOT_FOLDER}/{DATASET}/sparse/0/images.txt'
 images = []
 with open(IMAGE_PATH) as fp:
     lines = fp.readlines()[::2]
@@ -74,12 +101,22 @@ with open(IMAGE_PATH) as fp:
         images[idx] = {
             'name':name.rstrip('.png'),
             'image_id':image_id, 'camera_id':camera_id,
-            'quaternion': [qw,qx,qy,qz],
+            #JPL convention for scipy usage
+            'quaternion': [qx,qy,qz,qw],
             'translation': [tx,ty,tz],
         }
 
+## load point cloud
+print('load point cloud ...')
+PLY_PATH = f'{ROOT_FOLDER}/{DATASET}/sparse/0/points3D.ply'
+ply_pcd = o3d.io.read_point_cloud(PLY_PATH)
+pcd = o3d.geometry.PointCloud()
+pcd.points = ply_pcd.points
+pcd.colors = ply_pcd.colors
+# visualize_trajectory(pcd, images); exit()
+
 ## crop point cloud w.r.t images
-CHOP_PATH = Path('dataset/%s/sparse/chopped'%DATASET).resolve()
+CHOP_PATH = Path( f'{ROOT_FOLDER}/{DATASET}/sparse/chopped').resolve()
 CHOP_PATH.mkdir(exist_ok=True)
 for image in tqdm(images, 'crop point cloud to images'):
     # bypass generated
@@ -90,12 +127,12 @@ for image in tqdm(images, 'crop point cloud to images'):
     tw, th = camera['aspect_w'], camera['aspect_h']
     pyramid = np.array([
         [0,0,0],
-        [1,tw,th], [1,-tw,-th],
-        [1,-tw,th], [1,tw,-th],
+        [tw,th,1], [-tw,-th,1],
+        [-tw,th,1], [tw,-th,1],
     ]) * MAX_DEPTH
     # rotate and translate the pyramid
-    pyramid = R.from_quat(image['quaternion']).apply(pyramid)
-    pyramid += np.array(image['translation'])
+    pyramid -= np.array(image['translation'])
+    pyramid = R.from_quat(image['quaternion']).inv().apply(pyramid)
     # crop point cloud
     with tempfile.NamedTemporaryFile(mode='w+', suffix='.json') as fp:
         json.dump({
@@ -111,15 +148,15 @@ for image in tqdm(images, 'crop point cloud to images'):
         #
         vol = o3d.visualization.read_selection_polygon_volume(fp.name)
         chopped_pcd = vol.crop_point_cloud(pcd)
+        visualize_chopped_pcd(pcd, chopped_pcd, pyramid); exit()
         if len(chopped_pcd.points)==0:
             print(f'{output_file.name}: point cloud missing.')
-            # visualize_chopped_pcd(pcd, chopped_pcd, pyramid)
             continue
     ## hidden point removal
     diameter = np.linalg.norm( chopped_pcd.get_max_bound() - chopped_pcd.get_min_bound() )
     _, pt_map = chopped_pcd.hidden_point_removal(pyramid[0], diameter*100)
     chopped_pcd = chopped_pcd.select_by_index(pt_map)
-    # visualize_chopped_pcd(pcd, chopped_pcd, pyramid)
+    # visualize_chopped_pcd(pcd, chopped_pcd, pyramid); exit()
     o3d.io.write_point_cloud(output_file.as_posix(), chopped_pcd)
     image['pcd'] = output_file
     pass