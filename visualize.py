0c26d11f2d280cd3c2d8b88162fb0c1b00944082
iamhyc <sudofree@163.com>
2024-05-30 18:17:14 +0800

    feat: successive point removal

--- visualize.py
+++ visualize.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-import json
 import numpy as np
 import open3d as o3d
 from pathlib import Path
+from scipy.spatial import Delaunay
 from scipy.spatial.transform import Rotation as R
-import tempfile
 from tqdm import tqdm
 
 ROOT_FOLDER='/home/lab525/build/dataset' #'dataset'
 DATASET = 'wei'
 MAX_DEPTH = 10
+
+DEBUG_VISUALIZE = ['none',
+    'camera_trajectory', 'pyramid_crop', 'hidden_removal',
+    ][0]
 
 def monochromize(pcd, color):
     num_pcd = len(pcd.points)
@@ -113,16 +116,17 @@ ply_pcd = o3d.io.read_point_cloud(PLY_PATH)
 pcd = o3d.geometry.PointCloud()
 pcd.points = ply_pcd.points
 pcd.colors = ply_pcd.colors
-# visualize_trajectory(pcd, images); exit()
+if DEBUG_VISUALIZE=='camera_trajectory': visualize_trajectory(pcd, images)
 
 ## crop point cloud w.r.t images
 CHOP_PATH = Path( f'{ROOT_FOLDER}/{DATASET}/sparse/chopped').resolve()
 CHOP_PATH.mkdir(exist_ok=True)
 for image in tqdm(images, 'crop point cloud to images'):
-    # bypass generated
+    ## bypass generated
     output_file = CHOP_PATH / f'{image["name"]}.ply'
     if output_file.exists(): continue
-    # build inital pyramid
+    
+    ## build initial pyramid
     camera = cameras[ image['camera_id'] ]
     tw, th = camera['aspect_w'], camera['aspect_h']
     pyramid = np.array([
@@ -130,33 +134,37 @@ for image in tqdm(images, 'crop point cloud to images'):
         [tw,th,1], [-tw,-th,1],
         [-tw,th,1], [tw,-th,1],
     ]) * MAX_DEPTH
-    # rotate and translate the pyramid
+    ## rotate and translate the pyramid
     pyramid -= np.array(image['translation'])
     pyramid = R.from_quat(image['quaternion']).inv().apply(pyramid)
-    # crop point cloud
-    with tempfile.NamedTemporaryFile(mode='w+', suffix='.json') as fp:
-        json.dump({
-            'bounding_polygon': pyramid.tolist(),
-            'class_name': 'SelectionPolygonVolume',
-            'axis_min':min(pyramid[:,0]),
-            'axis_max':max(pyramid[:,0]),
-            'orthogonal_axis': 'X',
-            'version_major': 1,
-            'version_minor': 0,
-        }, fp)
-        fp.seek(0)
-        #
-        vol = o3d.visualization.read_selection_polygon_volume(fp.name)
-        chopped_pcd = vol.crop_point_cloud(pcd)
-        visualize_chopped_pcd(pcd, chopped_pcd, pyramid); exit()
-        if len(chopped_pcd.points)==0:
-            print(f'{output_file.name}: point cloud missing.')
-            continue
+    
+    ## crop pcd with pyramid bbox
+    pyramid_mesh = o3d.geometry.TriangleMesh(
+        vertices=o3d.utility.Vector3dVector(pyramid),
+        triangles=o3d.utility.Vector3iVector([
+            [0,1,3],[0,1,4],[0,2,3],[0,2,4],
+            [1,2,3],[1,2,4]
+        ])
+    )
+    bbox = pyramid_mesh.get_oriented_bounding_box()
+    chopped_pcd = pcd.crop(bbox)
+
+    ## crop pcd with convex hull
+    pyramid_hull = Delaunay(pyramid)
+    pt_map = np.where(
+        pyramid_hull.find_simplex(np.asarray(chopped_pcd.points)) >= 0
+    )[0]
+    chopped_pcd = chopped_pcd.select_by_index(pt_map)
+    if DEBUG_VISUALIZE=='pyramid_crop': visualize_chopped_pcd(pcd, chopped_pcd, pyramid)
+    if len(chopped_pcd.points)==0:
+        print(f'{output_file.name}: point cloud missing.')
+        continue
+
     ## hidden point removal
     diameter = np.linalg.norm( chopped_pcd.get_max_bound() - chopped_pcd.get_min_bound() )
     _, pt_map = chopped_pcd.hidden_point_removal(pyramid[0], diameter*100)
     chopped_pcd = chopped_pcd.select_by_index(pt_map)
-    # visualize_chopped_pcd(pcd, chopped_pcd, pyramid); exit()
+    if DEBUG_VISUALIZE=='hidden_removal': visualize_chopped_pcd(pcd, chopped_pcd, pyramid)
     o3d.io.write_point_cloud(output_file.as_posix(), chopped_pcd)
     image['pcd'] = output_file
     pass
